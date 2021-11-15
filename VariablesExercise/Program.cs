47fe0aad96a51db6839d36cef4fbaadec2afaac1
farrah24 <msfarrahd@gmail.com>
2021-11-14 20:51:50 -0600

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ralph";
+            int dogAge = 10;
+            char legs = 'l';
+            bool furry = true;
+            double tail = 5.3;
+            decimal eyes = 3.5m;
+            Console.WriteLine("string name = " + name);
+            Console.WriteLine("int dogAge = " + dogAge.ToString());
+            Console.WriteLine($"My dog's name is {name}, He is {dogAge} years old");
+
+         
+
+
+        }
+    }
+}
